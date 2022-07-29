6a7f9ce22cb5f31db77f1df520916f8edd52d548
Alex <tan.08.07.1997@gmail.com>
2022-07-29 17:54:46 +0700

    source view

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Singin from "./components/signin";
+import Singup from "./components/signup"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Singin /> */}
+      <Singup />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/signup/index.js
@@ -0,0 +1,244 @@
+import React from "react";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Link from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+
+export default function Signin() {
+  const [values, setValues] = React.useState({
+    password: "",
+    showPassword: false,
+    gilad: true,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChangeSwitch = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.checked,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundImage: 'url("https://wallpaperaccess.com/full/1994605.jpg")',
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "459px",
+          height: "492px",
+          background: "#FFFF",
+          borderRadius: "20px",
+          boxShadow: "0 35px 80px rgba(0,0,0,0.15)",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "-5%",
+            transform: "translateX(-50%)",
+            width: "427px",
+            height: "100.75px",
+            background: "#3a93ee",
+            borderRadius: "15px",
+          }}
+        >
+          <div
+            style={{
+              paddingTop: "10px",
+              textAlign: "center",
+              color: "#ffff",
+            }}
+          >
+            <div>
+              <div>
+                <h2>Sign Up</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div style={{ marginTop: "80px" }}>
+          <Stack
+            style={{
+              direction: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              spacing: "1",
+            }}
+          >
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3, marginLeft: "15px", marginRight: "15px" }}
+            >
+              <Grid container spacing={1}>
+                <Grid item xs={12} sm={6}>
+                  <FormControl sx={{ m: 1 }} variant="outlined">
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl sx={{ m: 1 }} variant="outlined">
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                    />
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </Box>
+            <Grid sm={{ marginTop: "5px" }}>
+              <Grid citem xs={12} md={12}>
+                <FormControl
+                  sx={{ m: 1, width: "412px", height: "44px" }}
+                  variant="outlined"
+                >
+                  <TextField
+                    id="outlined-textarea"
+                    label="Email"
+                    placeholder="abc@gmail.com"
+                    multiline
+                  />
+                </FormControl>
+              </Grid>
+              <Grid citem xs={12} md={12}>
+                <FormControl
+                  sx={{
+                    m: 1,
+                    width: "412px",
+                    height: "44px",
+                    marginTop: "20px",
+                  }}
+                  variant="outlined"
+                >
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Password
+                  </InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={values.showPassword ? "text" : "password"}
+                    value={values.password}
+                    onChange={handleChange("password")}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {values.showPassword ? (
+                            <VisibilityOff />
+                          ) : (
+                            <Visibility />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Password"
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Stack>
+
+          <Grid item xs={8} sx={{ marginTop: "20px", marginLeft: "15px" }}>
+            <FormControlLabel
+              control={<Checkbox value="allowExtraEmails" color="primary" />}
+              label="I want to receive inspiration, marketing promotions and updates via email."
+            />
+          </Grid>
+          <div
+            style={{
+              display: "block",
+              textAlign: "center",
+              marginTop: "10px",
+            }}
+          >
+            <div>
+              <Button
+                style={{
+                  width: "412px",
+                  height: "36px",
+                  background: "#3a93ee",
+                  marginTop: "21px",
+                }}
+                variant="contained"
+                disableElevation
+              >
+                sign up
+              </Button>
+              <Grid container justifyContent="flex-end" marginTop="15px">
+                <Grid item marginRight="20px">
+                  <Link href="../" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
